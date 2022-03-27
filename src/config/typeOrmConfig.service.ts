6c19867469f034dbd7fa07a41efdb864e24ac112
Ddudooo <igoa.work@gmail.com>
2022-03-27 17:43:20 +0900

    typeorm 설정 추가

---
+++ src/config/typeOrmConfig.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  @Inject(ConfigService)
+  private readonly config: ConfigService
+
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.config.get<string>('DATABASE_HOST'),
+      port: this.config.get<number>('DATABASE_PORT'),
+      database: this.config.get<string>('DATABASE_NAME'),
+      username: this.config.get<string>('DATABASE_USER'),
+      password: this.config.get<string>('DATABASE_PASSWORD'),
+      entities: ['src/**/*.entity.{ts,js}'],
+      migrations: ['src/db/migrations/*.ts'],
+      logging: 'all',
+      synchronize: true,
+    }
+  }
+}
