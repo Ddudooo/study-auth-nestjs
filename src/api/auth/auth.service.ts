f7ab056ceb327a309503a3091991b1610b306e0e
Ddudooo <igoa.work@gmail.com>
2022-03-29 00:29:19 +0900

    인증 서비스 추가

---
+++ src/api/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import { AuthProvider } from '@/api/auth/auth.provider'
+import { LoginDto } from '@/api/auth/dto/login.dto'
+import { RegisterDto } from '@/api/auth/dto/register.dto'
+import { User } from '@/api/user/user.entity'
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { plainToInstance } from 'class-transformer'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+    private readonly provider: AuthProvider,
+  ) {}
+
+  async register(body: RegisterDto): Promise<User | never> {
+    const { username, email, password, name } = body
+    const user: User = await this.repository.findOne({ where: { username } })
+
+    if (user) {
+      throw new ConflictException()
+    }
+
+    const registerUser = plainToInstance(User, {
+      username,
+      email,
+      name,
+      password: this.provider.encodePassword(password),
+    })
+
+    return this.repository.save(registerUser, { reload: true })
+  }
+
+  async login(loginDto: LoginDto): Promise<string | never> {
+    const { username, password } = loginDto
+    const user: User = await this.repository.findOne({ where: { username } })
+
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+
+    const isPasswordValid: boolean = await this.provider.isPasswordValid(
+      password,
+      user.password,
+    )
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException()
+    }
+
+    await this.repository.update(user.id, { lastLoginAt: new Date() })
+
+    return this.provider.generateToken(user)
+  }
+
+  async refreshToken(user: User): Promise<string> {
+    await this.repository.update(user.id, { lastLoginAt: new Date() })
+
+    return this.provider.generateToken(user)
+  }
+}
