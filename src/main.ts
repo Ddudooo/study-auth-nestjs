235e983eee5359843109883671a82415f8260a0c
Ddudooo <igoa.work@gmail.com>
2022-03-29 00:54:50 +0900

    어플리케이션 전역설정 업데이트

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { AppModule } from '@/app.module'
+import { setNestApp } from '@/config/setNestApp'
 import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  setNestApp(app)
+
   await app.listen(process.env.APPLICATION_PORT || 3000)
 }
 
